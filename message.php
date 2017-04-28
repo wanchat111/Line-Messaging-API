159780e69c4549b71b05c1ccda6ba1cc28b855fd
Wanchat Damdoung <wanchat111@hotmail.com>
2017-04-28 14:21:51 +0700

    Test

--- message.php
+++ message.php
@@ -15,6 +15,7 @@ if (!is_null($events['events'])) {
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
+			echo $replyToken;
 
 			// Build message to reply back
 			$messages = [
