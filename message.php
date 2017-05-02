6d1450a46310933c5dc49d94ec925c075cdfde94
Wanchat Damdoung <wanchat111@hotmail.com>
2017-05-02 09:24:03 +0700

    test

--- message.php
+++ message.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			//$text = $event['message']['text'];
+			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			$groupId = $event['source']['groupId'];
@@ -27,31 +27,30 @@ if (!is_null($events['events'])) {
 			fclose($fp);
 
 			// Build message to reply back
-			// $messages = [
-			// 	'type' => 'text',
-			// 	'text' => $text
-			// ];
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
-			// $url = 'https://api.line.me/v2/bot/message/reply';
-			// $data = [
-			// 	'replyToken' => $replyToken,
-			// 	'messages' => [$messages],
-			// ];
-			// $post = json_encode($data);
-			// $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			// $ch = curl_init($url);
-			// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			// curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			// $result = curl_exec($ch);
-			// curl_close($ch);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
 
-			// echo $result . "\r\n";
-
+			echo $result . "\r\n";
 		}
 	}
 }
