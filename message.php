dd7189878c2276f2c7bc52f7a3bbc5e110508bb2
Wanchat Damdoung <wanchat111@hotmail.com>
2017-05-02 09:26:23 +0700

    Test

--- message.php
+++ message.php
@@ -10,10 +10,9 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		    //$groupId = $event['source']['groupId'];
-			$fp2 = fopen('testjoin.txt', 'w');
-			//fwrite($fp, $replyToken);
-			fwrite($fp2, "555");
-			fclose($fp2);
+			// $fp2 = fopen('testjoin.txt', 'w');
+			// fwrite($fp2, "555");
+			// fclose($fp2);
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
